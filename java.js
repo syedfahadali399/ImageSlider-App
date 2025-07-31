7980b36169b13b39b2a5bbff1dc9991b52d30bb0
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-07-31 21:59:54 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,73 @@
+let showImage = document.getElementById("showImage")
+
+let imageArray = [
+    "pics/image1.jpg",
+    "pics/image2.jpg",
+    "pics/image3.jpg",
+    "pics/image4.jpg",
+    "pics/image5.jpg",
+    "pics/image6.jpg",
+]
+
+let currIndex = 0
+let numberOfImageInArray = imageArray.length - 1
+
+function imageSliderIncrease() {
+    currIndex++  
+    showImage.src = imageArray[currIndex] 
+    showImage.classList.add("active")
+    if(currIndex > numberOfImageInArray) {
+        currIndex = 0
+        showImage.src = imageArray[currIndex]
+    }
+}
+
+function imageSliderDecrease() {
+    currIndex--
+    showImage.src = imageArray[currIndex] 
+    showImage.classList.add("active")
+    if(currIndex < 0) {
+        currIndex = numberOfImageInArray
+        showImage.src = imageArray[currIndex]
+    }
+}
+
+function firstImage() {
+    showImage.src = imageArray[0]
+    showImage.classList.add("active")
+    setTimeout(() => {
+        showImage.classList.remove("active")
+    }, 1000)
+}
+
+firstImage()
+
+document.getElementById("next").addEventListener("click", function() {
+    imageSliderIncrease()
+    setTimeout(() => {
+        showImage.classList.remove("active")
+    }, 1000)
+    console.log(currIndex);    
+})
+
+document.getElementById("previous").addEventListener("click", function() {
+    imageSliderDecrease()   
+    setTimeout(() => {
+        showImage.classList.remove("active")
+    }, 1000) 
+    console.log(currIndex);
+    
+})
+
+document.getElementById("auto").addEventListener("click", function() {
+    setInterval(() => {
+        imageSliderIncrease()
+        setTimeout(() => {
+            showImage.classList.remove("active")
+        }, 1000) 
+    },3000)
+})
+
+document.getElementById("reset").addEventListener("click", function() {
+    firstImage()
+})
